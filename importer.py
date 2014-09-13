e84007aec28a70801be0270998219eb5a8108fbe
Pavel Kral <pavel@pavelkral.eu>
2014-09-13 12:59:43 +0200

    Added mavlink log file

--- importer.py
+++ importer.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python
 
 log_file = "./mavlink-log.txt"
+
+def get_first_timestamp(log_file):
+	with open(log_file) as f:
+		for line in f:
+			print line
+			return
+
+with open(log_file) as f:
+	for line in f:
+		print line
