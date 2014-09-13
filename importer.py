7b9de74eb09d4e5543f6649e3aa468ad0591f94b
Pavel Kral <pavel@pavelkral.eu>
2014-09-13 13:19:48 +0200

    Importer 1.0

--- importer.py
+++ importer.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python
 
+import json
+import sys
+import time
+
 log_file = "./mavlink-log.txt"
+
+
 
 def get_first_timestamp(log_file):
 	with open(log_file) as f:
 		for line in f:
-			print line
-			return
+			line_json = json.loads(line)
+			return line_json["timestamp"]
+
+
+start_time_importer = time.time()
+start_time_file = get_first_timestamp(log_file)
+
 
 with open(log_file) as f:
 	for line in f:
-		print line
+		line_json = json.loads(line)
+
+		importer_age = time.time() - start_time_importer
+		line_age = line_json["timestamp"] - start_time_file
+
+		sleep_time = abs(line_age - importer_age)
+
+		print(sleep_time)
+		time.sleep(sleep_time)
